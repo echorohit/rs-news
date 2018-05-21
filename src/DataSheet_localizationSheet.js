428d9e2ef1452749507cced1d4335c0f7dfc071c
Rohit Choudhary <rohit_choudhary@mckinsey.com>
2018-05-21 11:52:23 +0530

    Initial Commit

---
+++ src/DataSheet_localizationSheet.js
@@ -0,0 +1,32 @@
+import DataSheetBase from './DataSheetBase.js';
+
+export default class DataSheet_localizationSheet extends DataSheetBase {
+
+  constructor(id, updateCb) {
+    super(id, updateCb);
+    this.requestedKeyPath = "";  // this value can be specified in the React Studio data sheet UI
+  }
+
+  makeDefaultItems() {
+    // eslint-disable-next-line no-unused-vars
+    let key = 1;
+    // eslint-disable-next-line no-unused-vars
+    let item;
+  }
+
+  getStringsByLanguage = () => {
+    let stringsByLang = {};
+    for (let row of this.items) {
+      const locKey = row.key;
+      for (let key in row) {
+        if (key === 'key')
+          continue;
+        let langObj = stringsByLang[key] || {};
+        langObj[locKey] = row[key];
+        stringsByLang[key] = langObj;
+      }
+    }
+    return stringsByLang;
+  }
+
+}
---
+++ src/StartScreen.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+export default class StartScreen extends Component {
+
+  // Properties used by this component:
+  // appActions, deviceInfo
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    let baseStyle = {};
+    // eslint-disable-next-line no-unused-vars
+    let layoutFlowStyle = {};
+    if (this.props.transitionId && this.props.transitionId.length > 0 && this.props.atTopOfScreenStack && this.props.transitionForward) {
+      baseStyle.animation = '0.25s ease-in-out '+this.props.transitionId;
+    }
+    if ( !this.props.atTopOfScreenStack) {
+      layoutFlowStyle.height = '100vh';
+      layoutFlowStyle.overflow = 'hidden';
+    }
+    
+    const style_background = {
+        width: '100%',
+        height: '100%',
+     };
+    const style_background_outer = {
+        backgroundColor: '#f4f4f4',
+        pointerEvents: 'none',
+     };
+    
+    return (
+      <div className="AppScreen StartScreen" style={baseStyle}>
+        <div className="background">
+          <div className='appBg containerMinHeight elBackground' style={style_background_outer}>
+            <div style={style_background} />
+          
+          </div>
+          
+        </div>
+      </div>
+    )
+  }
+  
+
+}
